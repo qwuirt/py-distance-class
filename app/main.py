9645a60672856701fd27e76e6b35083c7d3bfc7d
Ihor <ihor.hrymut@gmail.com>
2026-02-01 11:14:09 +0100

    Solution++

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-from typing import Any, Callable
+from __future__ import annotations
 
 
 class Distance:
 
-    def __init__(self, km: Any) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,21 +12,21 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float | Callable) -> "Distance":
+    def __add__(self, other: int | float | Distance) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         raise TypeError("TypeError")
 
-    def __iadd__(self, other: int | float | Callable) -> "Distance":
+    def __iadd__(self, other: int | float | Distance) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
+            return self
         elif isinstance(other, (int, float)):
             self.km += other
-        else:
-            raise TypeError("TypeError")
-        return self
+            return self
+        raise TypeError("TypeError")
 
     def __mul__(self, other: int | float) -> "Distance":
         if isinstance(other, (int, float)):
@@ -38,35 +38,35 @@ class Distance:
             return Distance(round((self.km / other), 2))
         raise TypeError("TypeError")
 
-    def __lt__(self, other: int | float | Callable) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
         raise TypeError("TypeError")
 
-    def __gt__(self, other: int | float | Callable) -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
         raise TypeError("TypeError")
 
-    def __eq__(self, other: int | float | Callable) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
         raise TypeError("TypeError")
 
-    def __le__(self, other: int | float | Callable) -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
         raise TypeError("TypeError")
 
-    def __ge__(self, other: int | float | Callable) -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
